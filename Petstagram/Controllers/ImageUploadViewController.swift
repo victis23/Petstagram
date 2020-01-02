652d6e36345738e67a24abc3c22516884aba31d8
Michael Wells <wow@duhmarket.com>
2020-01-01 21:21:05 -0500

    Create struct object for housing images Create section enum for housing sections Add variables needed for selected image and collection view Create collection property that holds UserImages Object Create dataSource For CollectionView Add rightBarButtonFor Camera Action Create DataSource Create Snapshot Create instance of ImagePicker

--- Petstagram/Controllers/ImageUploadViewController.swift
+++ Petstagram/Controllers/ImageUploadViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
+struct UserImages : Hashable, Identifiable {
+	var images : UIImage = UIImage()
+	var id = UUID().uuidString
+	
+	func hash(into hasher :inout Hasher){
+		hasher.combine(id)
+	}
+}
+
 class ImageUploadViewController: UIViewController {
+	
+	enum Sections {
+		case main
+	}
+	
+	@IBOutlet weak var selectedImage: UIImageView!
+	@IBOutlet weak var albumImageCollection: UICollectionView!
+	
+	private var albumImages : [UserImages] = [] {
+		didSet {
+			createSnapshot(images: albumImages)
+		}
+	}
+	
+	private var datasource : UICollectionViewDiffableDataSource<Sections,UserImages>!
+	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -17,16 +42,46 @@ class ImageUploadViewController: UIViewController {
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
+		setDataSource()
+		createSnapshot(images: albumImages)
 		selectImageWithPicker()
 	}
-	
 	
 	func setNavigationBar(){
 		self.navigationItem.title = "Petstagram"
 		self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : UIFont(name: "Billabong", size: 35)!]
 		self.navigationController?.navigationBar.tintColor = .label
+		self.navigationItem.rightBarButtonItems = [UIBarButtonItem(image: UIImage.init(systemName: "camera.circle"), style: .plain, target: self, action: #selector(cameraButtonSelected))]
 	}
 	
+	@objc func cameraButtonSelected(){
+		
+	}
+
+}
+
+extension ImageUploadViewController: UICollectionViewDelegate {
+	
+	func setDataSource(){
+		datasource = UICollectionViewDiffableDataSource<Sections,UserImages>(collectionView: albumImageCollection, cellProvider: { (collectionView, indexPath, images) -> UICollectionViewCell? in
+			guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ImagesCollectionViewCell else {fatalError()}
+			
+			cell.imageFromAlbum.image = images.images
+			cell.imageFromAlbum.contentMode = .scaleAspectFill
+			
+			return cell
+		})
+	}
+	
+	func createSnapshot(images : [UserImages]){
+		var snapshot = NSDiffableDataSourceSnapshot<Sections,UserImages>()
+		snapshot.appendSections([.main])
+		snapshot.appendItems(images, toSection: .main)
+		
+		datasource.apply(snapshot, animatingDifferences: true, completion: {
+			
+		})
+	}
 }
 
 extension ImageUploadViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
@@ -38,12 +93,24 @@ extension ImageUploadViewController: UINavigationControllerDelegate, UIImagePick
 
 		if UIImagePickerController.isSourceTypeAvailable(.camera) && UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
 			imagePickerController.sourceType = .photoLibrary
+			imagePickerController.allowsEditing = true
 			
 			present(imagePickerController, animated: true)
 		}
 
 	}
-
-
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		
+		guard let rectangleImage = info[.editedImage] as? UIImage else {return}
+		
+		selectedImage.image = rectangleImage
+		selectedImage.contentMode = .scaleAspectFill
+		albumImages.append(UserImages(images: rectangleImage))
+		dismiss(animated: true, completion: {})
+		
+	}
+	
+	
 }
 
