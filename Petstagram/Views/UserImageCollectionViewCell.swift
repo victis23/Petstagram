a3dcca461185991cf3c6d12e8681720ce461f3a9
Michael Wells <wow@duhmarket.com>
2020-01-04 18:24:08 -0500

    create class for cell in user profile

---
+++ Petstagram/Views/UserImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserImageCollectionViewCell.swift
+//  Petstagram
+//
+//  Created by Scott Leonard on 1/4/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+class UserImageCollectionViewCell : UICollectionViewCell {
+	
+	@IBOutlet weak var imageCell : UIImageView!
+	
+}
