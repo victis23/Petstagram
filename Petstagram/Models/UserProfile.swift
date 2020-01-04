7a80fcb83087a384f5a648e43e56de146bbd65b5
Michael Wells <wow@duhmarket.com>
2020-01-04 01:11:28 -0500

    create singleton to hold user images and username

---
+++ Petstagram/Models/UserProfile.swift
@@ -0,0 +1,27 @@
+//
+//  UserProfile.swift
+//  Petstagram
+//
+//  Created by Scott Leonard on 1/4/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import Foundation
+
+struct UserProfile {
+	
+	var username : String?
+	var images : [Data]?
+	
+	static private var sharedUserProfile = UserProfile()
+	
+	private init(username: String? = "", images: [Data]? = []){
+		self.username = username
+		self.images = images
+	}
+	
+	static func shared() -> UserProfile {
+		return sharedUserProfile
+	}
+	
+}
