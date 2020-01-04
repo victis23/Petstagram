ec608026cd194176e8ffec98225e1ce7a6db76be
Michael Wells <wow@duhmarket.com>
2020-01-04 18:24:48 -0500

    add sections and image holding object add collection for images add datasource and snapshot methods

--- Petstagram/Controllers/UserProfileViewController.swift
+++ Petstagram/Controllers/UserProfileViewController.swift
@@ -10,17 +10,65 @@ import UIKit
 
 class UserProfileViewController: UIViewController {
 	
+	enum Sections {
+		case main
+	}
+	
+	struct UserProfileImageCollection : Hashable, Identifiable {
+		var image : UIImage
+		var id = UUID().uuidString
+		
+	}
+	
 	
 	@IBOutlet weak var accountImages: UICollectionView!
 	
 	let appDelegate = UIApplication.shared.delegate as! AppDelegate
 	let coreDataModel = AuthenticationItems(context: (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext)
 	
+	var datasource : UICollectionViewDiffableDataSource<Sections,UserProfileImageCollection>!
+	
+	var images : [UserProfileImageCollection] = [] {
+		didSet {
+			setSnapShot()
+		}
+	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
 		setNavigationBar()
+		setDataSource()
+		setCollectionViewLayout()
     }
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		getUserImageData()
+	}
+	
+	func getUserImageData(){
+		
+		self.images = []
+		let accountImages = UserProfile.shared().images
+		
+		accountImages?.forEach({ imageData in
+			self.images.append(UserProfileImageCollection(image: UIImage(data: imageData) ?? UIImage()))
+		})
+	}
+	
+	func setCollectionViewLayout(){
+		
+		let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+		let cell = NSCollectionLayoutItem(layoutSize: itemSize)
+		
+		let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5))
+		let cellGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: cell, count: 4)
+		
+		let section = NSCollectionLayoutSection(group: cellGroup)
+		
+		let layout = UICollectionViewCompositionalLayout(section: section)
+		accountImages.collectionViewLayout = layout
+	}
     
 	func setNavigationBar(){
 		self.navigationItem.title = "Petstagram"
@@ -28,6 +76,28 @@ class UserProfileViewController: UIViewController {
 		self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "square.stack.3d.up"), style: .plain, target: self, action: #selector(temporaryMethodForLoggingOut))
 		self.navigationController?.navigationBar.tintColor = .label
 	}
+	
+	func setDataSource(){
+		datasource = UICollectionViewDiffableDataSource<Sections,UserProfileImageCollection>(collectionView: accountImages, cellProvider: { (collectionView, indexPath, ImageObject) -> UICollectionViewCell? in
+			
+			guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "image", for: indexPath) as? UserImageCollectionViewCell else {fatalError()}
+			
+			cell.imageCell.image = ImageObject.image
+			
+			return cell
+		})
+	}
+	
+	func setSnapShot(){
+		var snapShot = NSDiffableDataSourceSnapshot<Sections,UserProfileImageCollection>()
+		snapShot.appendSections([.main])
+		snapShot.appendItems(self.images, toSection: .main)
+		datasource.apply(snapShot, animatingDifferences: false, completion: {
+			
+		})
+	}
+	
+	
 	
 	@objc func temporaryMethodForLoggingOut(){
 		
