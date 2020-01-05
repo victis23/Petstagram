af06014ba0dce197949e016627379d618544bd51
Michael Wells <wow@duhmarket.com>
2020-01-04 21:35:05 -0500

    Add new keys for googlefirestore

--- Petstagram/KeyModel/Keys.swift
+++ Petstagram/KeyModel/Keys.swift
@@ -13,8 +13,16 @@ struct Keys {
 		static var accessSegue = "accessSegue"
 		static var signOut = "unwindToLogin"
 	}
-	struct errorKeys {
+	struct ErrorKeys {
 		static var loginFailed = "loginError"
 		static var accountCreationFailed = "failedCreation"
+	}
+	struct GoogleFireStore {
+		static var accountInfoDocument = "accountInfo"
+		static var usernameKey = "Username"
+		static var userCollection = "Users"
+		static var userKeysDocument = "UserKeys"
+		static var accountImagesDocument = "ImagesDocument"
+		static var images = "images"
 	}
 }
