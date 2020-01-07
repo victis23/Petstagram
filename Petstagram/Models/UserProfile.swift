77ec5bb2e80d80abd30de9fe75ed154b2a9d010d
Michael Wells <wow@duhmarket.com>
2020-01-06 21:46:13 -0500

    Add property that holds list of image references in database. Add method that uploads images to storage and also saves list of names. Add method that downloads images from storage based on list of names.

--- Petstagram/Models/UserProfile.swift
+++ Petstagram/Models/UserProfile.swift
@@ -18,6 +18,8 @@ class UserProfile {
 	let user = Auth.auth().currentUser?.uid
 	let db = Firestore.firestore()
 	let storage = Storage.storage()
+	var imageReferences : [String] = []
+	
 	
 	static private var sharedUserProfile = UserProfile()
 	
@@ -28,57 +30,72 @@ class UserProfile {
 		print("New Instance \(self.username ?? "No Username") | \(self.imageData ?? [])")
 	}
 	
-	func saveImageDataToCloud(){
-		guard let user = self.user else {fatalError()}
-		guard let images = self.imageData else {fatalError()}
-		
-		db.collection(user).document(Keys.GoogleFireStore.accountImagesDocument).setData([
-			Keys.GoogleFireStore.images : images
-		], merge: true, completion: {_ in
-			
-			print("\(images) have been uploaded to Google FireStore!")
-			
-		})
-		
-	}
+//	func retrieveListOnLoad(){
+//		guard let user = self.user else {fatalError()}
+//
+//		db.collection(user).document(Keys.GoogleFireStore.accountImagesDocument).getDocument { (document, error) in
+//			if let error = error {
+//				print(error.localizedDescription)
+//				return
+//			}
+//			guard let imageKeys = document else {fatalError()}
+//
+//			if imageKeys.data() != nil {
+//				return
+//			}else{
+//				self.imageReferences.append(contentsOf: imageKeys["imageKeys"] as! [String])
+//			}
+//		}
+//	}
 	
 	func uploadDataToFireBase(){
 		guard let user = self.user else {fatalError()}
 		guard let imageData = self.imageData else {fatalError()}
 		
+		let dataFormat = DateFormatter()
+		dataFormat.dateStyle = .long
+		
+		
 		let root = storage.reference()
-		let reference = root.child(Keys.GoogleStorage.imageDataArray)
 		
-	
+		imageData.enumerated().forEach({ count , data in
+			let imgRef = "\(Keys.GoogleStorage.imageDataArray)\(dataFormat.string(from: Date()).hashValue)\(count)"
+			imageReferences.append(imgRef)
+			let reference = root.child(user).child(imgRef)
+			reference.putData(data)
+		})
+		db.collection(user).document(Keys.GoogleFireStore.accountImagesDocument).setData([
+			"imageKeys" : imageReferences
+		], merge: false, completion: { _ in})
 	}
 	
-	func getImagesFromCloud() {
-		
+	func downloadDataFromFireBase() {
 		guard let user = self.user else {fatalError()}
+		let file = storage.reference().child(user)
+		var retrievedData : [Data] = []
 		
-		db.collection(user)
-			.document(Keys.GoogleFireStore.accountImagesDocument)
-			.getDocument(source: .default) { (data, error) in
-				if let error = error {
-					print(error.localizedDescription)
-					return
+		db.collection(user).document(Keys.GoogleFireStore.accountImagesDocument).getDocument { (document, error) in
+			if let error = error {
+				print(error.localizedDescription)
+				return
+			}
+			guard let document = document else {fatalError()}
+			guard let imageList = document["imageKeys"] as? [String] else {fatalError()}
+			
+			imageList.forEach { name in
+				file.child(name).getData(maxSize: 9_999_999_999) { (data, error) in
+					if let error = error {
+						print(error.localizedDescription)
+						return
+					}
+					guard let data = data else {fatalError()}
+					retrievedData.append(data)
 				}
-				
-				guard let data = data else {fatalError()}
-				self.imageData = data[Keys.GoogleFireStore.images] as? [Data]
-				
+			}
+			self.imageData = retrievedData
 		}
-		
-//		db.collection(user).document(Keys.GoogleFireStore.accountImagesDocument).addSnapshotListener(includeMetadataChanges: true) { (snapShot, error) in
-//			if let error = error {
-//				print(error.localizedDescription)
-//				return
-//			}
-//			snapShot.flatMap({
-//				self.imageData = $0[Keys.GoogleFireStore.images] as? [Data]
-//			})
-//		}
 	}
+	
 	
 	static func shared() -> UserProfile {
 		return sharedUserProfile
