e76cb2e795c55405ee88bbcb64a61fbe5d011f59
Michael Wells <wow@duhmarket.com>
2020-01-11 14:54:03 -0500

    create file for userprofileimagecollection struct

---
+++ Petstagram/Models/UserProfileImageCollectionModel.swift
@@ -0,0 +1,22 @@
+//
+//  UserProfileImageCollectionModel.swift
+//  Petstagram
+//
+//  Created by Scott Leonard on 1/11/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+struct UserProfileImageCollection : Hashable, Identifiable, Equatable {
+	var image : UIImage
+	var id :String
+	
+	func hash(into hasher : inout Hasher) {
+		hasher.combine(id)
+	}
+	
+	static func ==(lhs:UserProfileImageCollection, rhs: UserProfileImageCollection) -> Bool {
+		lhs.id == rhs.id
+	}
+}
