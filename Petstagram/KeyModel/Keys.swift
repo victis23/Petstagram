5e99af148ce2882473e3bf47203b6bd77a665a4c
Michael Wells <wow@duhmarket.com>
2020-01-17 15:50:34 -0500

    documentation

--- Petstagram/KeyModel/Keys.swift
+++ Petstagram/KeyModel/Keys.swift
@@ -8,16 +8,23 @@
 
 import Foundation
 
+/// Contains keys for various objects and operations. 
 struct Keys {
+	
+	// Keys that handle segue actions.
 	struct Segues {
 		static var accessSegue = "accessSegue"
 		static var signOut = "unwindToLogin"
 		static var imageViewer = "imageViewer"
 	}
+	
+	// Keys that handle errors.
 	struct ErrorKeys {
 		static var loginFailed = "loginError"
 		static var accountCreationFailed = "failedCreation"
 	}
+	
+	// Keys for Google Firebase items.
 	struct GoogleFireStore {
 		static var accountInfoDocument = "accountInfo"
 		static var usernameKey = "Username"
@@ -26,16 +33,20 @@ struct Keys {
 		static var accountImagesDocument = "ImagesDocument"
 		static var images = "images"
 	}
+	
+	// Keys for Google Firebase Storage.
 	struct GoogleStorage {
 		static var imageDataArray = "ImageDataArray"
 	}
 	
+	// Keys for userDefaults Database.
 	struct userDefaultsDB {
 		static var profilePhoto = "profilePhoto"
 		static var username = "userName"
 		static var imageCount = "imageCount"
 	}
 	
+	// Keys for secure keychain.
 	struct keyChainKeys {
 		static var email = "email"
 	}
