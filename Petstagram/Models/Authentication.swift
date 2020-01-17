3e76256d75e7b470ffb73a5d4012d30922da44f1
Michael Wells <wow@duhmarket.com>
2020-01-17 17:21:45 -0500

    documentation.

--- Petstagram/Models/Authentication.swift
+++ Petstagram/Models/Authentication.swift
@@ -10,11 +10,19 @@ import Foundation
 import FirebaseAuth
 import FirebaseFirestore
 
+/// Handles Authentication with Firebase & Updates Database for new users.
 struct Authentication {
 	var email :String
 	var password :String
 	var userName :String?
 	
+	
+	/// Principal method used for authentication of users — Honestly, probably violates SOLID
+	/// - Parameters:
+	///   - isRegistration: Bool that determines if user is creating a new account or signing in.
+	///   - isError: Captures Errors if any.
+	///   - segue: Captures `AuthDataResult` and allows caller to use it possibly in a segue.
+	/// - Important: This method also **saves user creds to keychain and username to userDefaults**.
 	func firebaseUserRegistration(isRegistration: Bool,isError: @escaping (_ error : Error)->(Void) ,segue : @escaping (_ result: AuthDataResult)->())throws{
 		
 		let firebaseAuthentication = Auth.auth()
@@ -28,8 +36,11 @@ struct Authentication {
 				}
 				guard let result = result else {return}
 				
+				// Saves user email to userDefaults
 				let userDefaults = UserDefaults()
 				userDefaults.set(self.email, forKey: Keys.keyChainKeys.email)
+				
+				// Saves user creds to keychain.
 				Authentication.saveCredsToKeyChain(using: self.email, password: self.password)
 				segue(result)
 			}
@@ -41,8 +52,11 @@ struct Authentication {
 				}
 				guard let result = result else {return}
 				
+				// Saves user email to userDefaults
 				let userDefaults = UserDefaults()
 				userDefaults.set(self.email, forKey: Keys.keyChainKeys.email)
+				
+				// Saves user creds to keychain.
 				Authentication.saveCredsToKeyChain(using: self.email, password: self.password)
 				segue(result)
 			}
@@ -50,6 +64,9 @@ struct Authentication {
 		
 	}
 	
+	
+	/// Writes username to file in database that houses the user's account information
+	/// - Important: Also writes to the database's main file that houses a list of all usernames and their cooresponding userIDs.
 	func createUserNameOnServer(){
 		
 		guard let userName = userName else {return}
@@ -57,8 +74,7 @@ struct Authentication {
 		
 		let db = Firestore.firestore()
 		
-		// Adds document that will hold information for individual users.
-		
+		// Writes
 		db.collection(userID).document(Keys.GoogleFireStore.accountInfoDocument).setData([
 			Keys.GoogleFireStore.usernameKey : userName
 		]) { (error) in
@@ -67,8 +83,7 @@ struct Authentication {
 			}
 		}
 		
-		// Adds new user information to document containing list of all existing users.
-		
+		//Writes username to database file that has keys for Usernames:ID's
 		db.collection(Keys.GoogleFireStore.userCollection).document(Keys.GoogleFireStore.userKeysDocument).setData([
 			userName:userID
 		], merge: true) { error in
