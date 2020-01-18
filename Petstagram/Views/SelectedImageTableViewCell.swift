94e1b6e28b7a4190726b8d87b179e4ccb8ec7193
Michael Wells <wow@duhmarket.com>
2020-01-18 17:34:09 -0500

    Add cell file.

---
+++ Petstagram/Views/SelectedImageTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SelectedImageTableViewCell.swift
+//  Petstagram
+//
+//  Created by Scott Leonard on 1/18/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+/// Cell in tableview controlled by AccountImageListTableViewController.
+class SelectedImageTableViewCell : UITableViewCell {
+	
+}
