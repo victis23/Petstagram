380eff1782dba003908045cd7d786a778ec3e075
Michael Wells <wow@duhmarket.com>
2020-02-01 00:09:32 -0500

    extract view into extension

--- Petstagram/Controllers/Extensions/UserProfileViewController/EditProfileDescription+UserProfileViewController.swift
+++ Petstagram/Controllers/Extensions/UserProfileViewController/EditProfileDescription+UserProfileViewController.swift
@@ -10,4 +10,33 @@ import UIKit
 
 extension UserProfileViewController {
 	
+	func createEditProfileDescriptionView()-> EditProfileDescription? {
+		
+		// Creates instance variable for xib file containing view for editing profile.
+		let editProfileView = Bundle.main.loadNibNamed("EditProfileView", owner: self, options: nil)
+		
+		// Downcast from Any to subclass of UIView.
+		let editView = editProfileView?.first as? EditProfileDescription
+		
+		editView?.layer.cornerRadius = 5
+		editView?.frame = CGRect(x: 0, y: 0, width: 300, height: 500)
+		editView?.center = view.center
+		editView?.submitButton.layer.cornerRadius = 5
+		
+		if let description = editView?.profileDescription {
+			
+			let color = UIColor(white: 0.9, alpha: 0.2)
+			description.text = ""
+			description.backgroundColor = color
+			description.textColor = .black
+			description.layer.cornerRadius = 5
+			description.becomeFirstResponder()
+		}
+		
+		return editView
+	}
+	
+	@objc func swipeToDismiss(_ sender: UIPanGestureRecognizer){
+		
+	}
 }
