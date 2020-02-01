4b512c795ff48fdcb97d64e23ba8bacbcfb38639
Michael Wells <wow@duhmarket.com>
2020-01-31 23:55:07 -0500

    add extension for handling editprofileview creation.

---
+++ Petstagram/Controllers/Extensions/UserProfileViewController/EditProfileDescription+UserProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EditProfileDescription+UserProfileViewController.swift
+//  Petstagram
+//
+//  Created by Scott Leonard on 1/31/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+extension UserProfileViewController {
+	
+}
