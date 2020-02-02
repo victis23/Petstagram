7b5f5274092ad3b32cd2f1c8d74584a80de119c3
Michael Wells <wow@duhmarket.com>
2020-02-02 10:52:53 -0500

    add subscription methods for combine

--- Petstagram/Controllers/UserProfileViewController.swift
+++ Petstagram/Controllers/UserProfileViewController.swift
@@ -80,7 +80,8 @@ class UserProfileViewController: UIViewController {
 		fetchDataFromCoreData()
 		setImageCount()
 		setSubscription()
-//		setDisableSubsciber()
+		disableParentView(isDisabled: isEditingProfileDetails)
+		setDisableSubsciber()
 		setCollectionViewLayout()
 	}
 	
@@ -311,10 +312,31 @@ class UserProfileViewController: UIViewController {
 		
 		view.addSubview(editView)
 		
-//		disableParentView(isDisabled: true)
+		disableParentView(isDisabled: true)
 		
 		editViewAnimations(subView: editView)
 		
+	}
+	
+	func setDisableSubsciber(){
+		isEditProfileSubscriber = isEditingDetails
+			.assign(to: \UIButton.isEnabled, on: editProfileInfoButton)
+	}
+	
+	func disableParentView(isDisabled:Bool){
+		
+		isEditingProfileDetails = isDisabled
+		
+		isEditingDetails = $isEditingProfileDetails
+			.map({ bool -> Bool in
+				if bool == true {
+					self.editProfileInfoButton.alpha = 0.2
+					return false
+				}
+				self.editProfileInfoButton.alpha = 1.0
+				return true
+			})
+			.eraseToAnyPublisher()
 	}
 	
 	//MARK: Navigation
