8ec93d1eaff0b9d0460e7a2c385aed5adf083bde
Michael Wells <wow@duhmarket.com>
2020-02-12 07:41:10 -0500

    extract model for application users.

---
+++ Petstagram/Models/PetstagramUsers.swift
@@ -0,0 +1,29 @@
+//
+//  PetstagramUsers.swift
+//  Petstagram
+//
+//  Created by Scott Leonard on 2/12/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+
+enum Section {
+	case main
+}
+
+struct PetstagramUsers : Hashable {
+	var username: String
+	var profileImage: UIImage?
+	var uid : String
+	
+	init(_ username : String,_ uid : String){
+		self.username = username
+		self.uid = uid
+	}
+	
+	func hash(into hasher : inout Hasher){
+		hasher.combine(uid)
+	}
+}
