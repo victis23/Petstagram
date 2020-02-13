a191143c78c465f9fe216ca94fc215f315c7a08c
Michael Wells <wow@duhmarket.com>
2020-02-13 07:51:01 -0500

    Add new controller to handle user accounts.

---
+++ Petstagram/Controllers/GenericProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GenericProfileViewController.swift
+//  Petstagram
+//
+//  Created by Scott Leonard on 2/13/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+class GenericProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
