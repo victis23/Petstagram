3afac981d99a70087f9c49a450a6a12a087aa121
Michael Wells <wow@duhmarket.com>
2020-02-13 00:42:12 -0500

    create cell file for account search table.

---
+++ Petstagram/Views/AccountSearchTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  AccountSearchTableViewCell.swift
+//  Petstagram
+//
+//  Created by Scott Leonard on 2/12/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+class AccountSearchTableViewCell: UITableViewCell {
+	
+	@IBOutlet weak var userName: UILabel!
+	@IBOutlet weak var profilePhoto : UIImageView!
+	@IBOutlet weak var followButton : UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
