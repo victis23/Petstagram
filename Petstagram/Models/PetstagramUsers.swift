f2221a6d8725e9a2dbd60e8eecf316f6236f08f6
Michael Wells <wow@duhmarket.com>
2020-02-15 14:27:10 -0500

    add property for following

--- Petstagram/Models/PetstagramUsers.swift
+++ Petstagram/Models/PetstagramUsers.swift
@@ -21,6 +21,7 @@ class PetstagramUsers : Hashable {
 	var username: String
 	var uid : String
 	var image : UIImage?
+	var following : Bool = false
 	
 	init(_ username : String,_ uid : String){
 		self.username = username
