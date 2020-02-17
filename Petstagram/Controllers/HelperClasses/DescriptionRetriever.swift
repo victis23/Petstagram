1d4820e5bdd22157ca91368344fd4644938c6522
Michael Wells <wow@duhmarket.com>
2020-02-17 07:54:20 -0500

    add new method for retrieving account username.

--- Petstagram/Controllers/HelperClasses/DescriptionRetriever.swift
+++ Petstagram/Controllers/HelperClasses/DescriptionRetriever.swift
@@ -28,6 +28,25 @@ class DescriptionRetriever {
 		}
 	}
 	
+	func getUserName(completion: @escaping (String)->Void){
+		
+		let defaults = UserDefaults()
+		
+		db.collection(userID).document(Keys.GoogleFireStore.accountInfoDocument).getDocument { (usernameDocument, error) in
+			if let error = error {
+				print(error.localizedDescription)
+			}
+			guard
+				let usernameDocument = usernameDocument,
+				let retrievedUserName = usernameDocument[Keys.GoogleFireStore.usernameKey] as? String
+				else {fatalError()}
+			
+			defaults.set(retrievedUserName, forKey: Keys.userDefaultsDB.username)
+			
+			completion(retrievedUserName)
+		}
+	}
+	
 	init(userID:String){
 		self.userID = userID
 	}
