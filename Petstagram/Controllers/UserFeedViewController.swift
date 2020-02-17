a076a0f643a2cd0f00d446379a09b7dfbac4ced9
Michael Wells <wow@duhmarket.com>
2020-02-17 11:29:37 -0500

    add extension that handles snapshot and datasource

--- Petstagram/Controllers/UserFeedViewController.swift
+++ Petstagram/Controllers/UserFeedViewController.swift
@@ -25,4 +25,32 @@ class UserFeedViewController: UIViewController {
 		self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : UIFont(name: "Billabong", size: 34)!]
 		
 	}
+	
+}
+
+extension UserFeedViewController : UITableViewDelegate {
+	
+	
+	// MARK: TableView & collectionView Methods
+	
+	func setDataSource(){
+		tableViewDataSource = UITableViewDiffableDataSource<Section,AccountImages>(tableView: tableView, cellProvider: { (tableView, indexPath, profileImages) -> UITableViewCell? in
+			let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+			return cell
+		})
+		
+		collectionViewDataSource = UICollectionViewDiffableDataSource<Section,PetstagramUsers>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, account) -> UICollectionViewCell? in
+			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+			return cell
+		})
+	}
+	
+	func tableViewSnapShot(){
+		
+	}
+	
+	func collectionViewSnapShot(){
+		
+	}
+	
 }
