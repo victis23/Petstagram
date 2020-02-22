dc4c7eb2878b087c0414589ed9dc890299f0f91c
Michael Wells <wow@duhmarket.com>
2020-02-22 16:17:42 -0500

    add properties to text class

--- Petstagram/PetstagramTests/PetstagramTests.swift
+++ Petstagram/PetstagramTests/PetstagramTests.swift
@@ -10,25 +10,20 @@ import XCTest
 @testable import Petstagram
 
 class PetstagramTests: XCTestCase {
+	
+	var descriptionReceiver : DescriptionRetriever!
+	var imageDownloader : ImageDownloader!
+	var collectionViewBuilder : CollectionViewBuilder!
+	var followerTracker : FollowerTracker!
+	var account = "fakeUser"
+	var username = "fakeUser"
+	var petstagramAccount : PetstagramUsers!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+		
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
