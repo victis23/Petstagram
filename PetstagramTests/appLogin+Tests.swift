19fa374be6f3f9253595c28dc43e1423a8564b48
Michael Wells <wow@duhmarket.com>
2020-02-23 06:43:50 -0500

    add another unit test file

---
+++ PetstagramTests/appLogin+Tests.swift
@@ -0,0 +1,22 @@
+//
+//  appLogin+Tests.swift
+//  PetstagramTests
+//
+//  Created by Scott Leonard on 2/23/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import XCTest
+@testable import Petstagram
+
+class appLogin_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
